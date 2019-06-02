2d11a01cb5e1914dc6a20b0d62635bbc05f52f1f
ghigbie <gehigbie@gmail.com>
2019-06-01 22:06:10 -0600

    Add new element

---
+++ lib/first_row.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FirstRow extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Container(
+        color: Colors.red,
+        height: 400.0,
+        width: double.infinity,
+      )
+    );
+  }
+}
