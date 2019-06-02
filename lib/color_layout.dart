c7b3388ec89a1225a563b9147cd2195455eefb1a
ghigbie <gehigbie@gmail.com>
2019-06-01 22:42:49 -0600

    Edit component

--- lib/color_layout.dart
+++ lib/color_layout.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-import 'first_row.dart';
+import 'color_layout.dart';
 
 class ColorLayout extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Center(
       child: Container(
-        child: FirstRow,
-      ));
+        padding: EdgeInsets.all(25),
+        color: Colors.grey[300],
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(color: Colors.red, height: 50, padding: EdgeInsets.only(bottom: 10)),
+            Container(color: Colors.blue, height: 40, padding: EdgeInsets.only(bottom: 10)) 
+          ]
+            ),
+        ),
+      );
+    }
   }
-}
